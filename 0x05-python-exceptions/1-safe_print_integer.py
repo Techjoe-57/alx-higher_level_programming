0b779bf054aae2bc32bb52613f1a027f63c5a082
Techjoe-57 <neequayejesse@gmail.com>
2024-01-09 07:22:42 -0800

    Fix pycodestyle validation error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-        try:
-            print("{:d}".format(value)) # Will only print if the datatype of 'value' is int
-            return True
-               
-        except Exception:
-            return False
+    try:
+        # Will only print if the datatype of 'value' is int
+        print("{:d}".format(value))
+        return True
+
+    except Exception:
+        return False
+
