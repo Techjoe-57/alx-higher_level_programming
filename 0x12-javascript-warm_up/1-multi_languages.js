be124018f0ef5558a194f43b967f592a0cc94d32
Techjoe-57 <techjoe57@gmail.com>
2024-06-19 16:04:31 -0700

    Create first script

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// JavaScript script to print 3 lines of output
+//
+let c_lang = 'C is fun';
+let python_lang = 'Python is cool';
+let js_lang = 'JavaScript is amazing';
+
+console.log(c_lang);
+console.log(python_lang);
+console.log(js_lang);
